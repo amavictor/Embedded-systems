c9365c39d6d2ce3a67cad8b4340b8cbb2027603e
chubiboo <43607068+chubiboo@users.noreply.github.com>
2021-02-05 08:12:42 +0100

    Add files via upload

---
+++ Serial Monitor/sketch_jan31a/sketch_jan31a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("My name is Ama Victor and my matric number is 16CJ020694");
+  delay(3000);
+}
